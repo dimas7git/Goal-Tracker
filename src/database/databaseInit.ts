ad0c39e30dbdcd37ae224e18a7186d200794a00a
Dimas F <dimaslimaferreira@gmail.com>
2024-03-06 12:41:46 -0300

    create database

---
+++ src/database/databaseInit.ts
@@ -0,0 +1,21 @@
+import { SQLiteDatabase } from "expo-sqlite/next";
+
+export async function databaseInit(database: SQLiteDatabase){
+    await database.execAsync(`
+        PRAGMA journal_mode= 'wal';
+
+        CREATE TABLE IF NOT EXISTS goals(
+            id INTEGER PRIMARY KEY NOT NULL,
+            name TEXT NOT NULL,
+            total REAL NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS transactions(
+            id INTEGER PRIMARY KEY NOT NULL,
+            amount REAL NOT NULL,
+            goal_id INTEGER NOT NULL,
+            created_at DEFAULT CURRENT_TIMESTAMP
+        );
+    `)
+}
+
